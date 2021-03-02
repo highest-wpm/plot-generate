0e0fbcbac856e1947b76d911a9ad2162f2d94f1e
Kuan Hui, Wu <tf00604520@gmail.com>
2021-03-02 15:22:39 +0800

    add app: plot_generate

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'plot_generate.apps.PlotGenerateConfig'
 ]
 
 MIDDLEWARE = [
---
+++ mysite/plot_generate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlotGenerateConfig(AppConfig):
+    name = 'plot_generate'
